0049db38c5fc3c107a6c7bca41cfb6478c96ec4c
Camila Alves Dias <cmdias@outlook.com.br>
2021-02-11 17:12:11 -0300

    scripts updated

--- merge.py
+++ merge.py
@@ -1,9 +1,11 @@
+# juntar (merge) dataframes/ csvs files
 import csv
 import pandas as pd
 import numpy as np
 
+# inputs
 df1 = pd.read_csv("relacoes-completas.csv")
 df2 = pd.read_csv("relacoes.csv")
 
 df3 = df1.merge(df2, on=["solicitacao_id"], how='outer')
-df3.to_csv("final.csv",index=False)
+df3.to_csv("final.csv",index=False) # output
